309a5e60e1d9c93774aede30842ef1816747ce26
lazycoderll <johntimmy320@gmail.com>
2023-03-05 00:05:18 +0100

    created navbar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,14 @@
+import Navbar from "scenes/navbar";
+import { Box } from "@mui/material"
+
+
+
 const HomePage = () => {
-    return (<div>HomePage</div>)
+    return (
+        <Box>
+            <Navbar />
+        </Box>
+    );
 }
 
-export default HomePage
+export default HomePage;
